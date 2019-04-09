a1da72380336cd31fc1849c2a6e6ad9f67b3412f
Patrick B Brown <pbbrown1112@gmail.com>
2019-04-09 19:29:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,14 +17,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string === hello {
+  if (string.toLowerCase() === string) {
     return `I can't hear you!`
   }
-  else if string === HELLO {
+  else if (string.toUpperCase() === STRING {
     return 'YES INDEED!'
   }
-  else if string === I love you, Grandma. {
+  else if (string === "I love you, Grandma." {
     return 'I love you, too!'
   }
-
 }
